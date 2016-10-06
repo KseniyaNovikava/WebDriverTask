1f56c72da4bc3b41cfb742614146e31720ab7705
Kseniya Novikava <kseniya_novikava@epam.com>
2016-10-06 15:30:15 +0300

    update webdriver task

--- onlinerSearch.js
+++ onlinerSearch.js
@@ -1,35 +1,80 @@
+    "use strict";
 
- 
-"use strict";
- 
-var webdriver = require('selenium-webdriver');
-var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
- 
+    var webdriver = require('selenium-webdriver');
+    var browser = new webdriver.Builder().usingServer().withCapabilities({'browserName': 'chrome' }).build();
 
- 
-function clickLink(link) {
-    link.click();
-}
- 
-function handleFailure(err) {
-    console.error('Something went wrong\n', err.stack, '\n');
-    closeBrowser();
-}
-function findAppleCheckbox() {
-    return browser.findElements(webdriver.By.css("span.i-checkbox__faux")).then(function(result) {
-        return result[0];
+    const assert = require('assert');
+    var until = webdriver.until;
+
+    function testPassed(text){
+        console.log(text);
+    }
+    function testFailed(err){
+        console.error(err.message);
+    }
+
+    browser.get('https://www.onliner.by');
+    browser.findElement(webdriver.By.css("input.fast-search__input")).
+    sendKeys("Apple ").
+    then(function(){
+
+        browser.switchTo().frame(browser.findElement(webdriver.By.css("iframe.modal-iframe")))
+    })
+    .then(function(){
+        return browser.wait(until.elementLocated(webdriver.By.css("input.search__input")));
+    })
+    .then(function(searchInput) {
+        searchInput.sendKeys('iPhone 6s 16GB Silver');
+    })
+    .then(function(){
+        return browser.wait(until.elementLocated(webdriver.By.css("a[href$='iphone6s16gbs']")));
+    })
+    .then(function(link) {
+        link.click();
+    })
+    .then(function(){
+        browser.wait(until.titleIs('Купить iPhone 6s'));
+    })
+    .then(function(){
+        return browser.findElement(webdriver.By.css('tbody:nth-child(3) > tr:last-child > td:last-child > span')).getText();        
+    })
+    .then(function(camera){
+        assert.equal(camera, "12 Мп","! CameraTest")
+        return "+ CameraTest";
+    }).then(testPassed,testFailed)
+
+    .then(function(){
+        return browser.findElement(webdriver.By.css('tbody:nth-child(4) > tr:nth-child(2) > td:last-child > span')).getText();  
+    })
+    .then(function(processor){
+        assert.equal(processor, "Apple A9","! ProcessorTest");
+        return "+ ProcessorTest"
+    }).then(testPassed,testFailed)
+
+    .then(function(){
+        return browser.findElement(webdriver.By.css('tbody:nth-child(5) > tr:nth-child(12) > td:last-child > span')).getAttribute('class'); 
+    })
+    .then(function(fingerprint){
+        assert.equal(fingerprint, "i-tip","! FingerprintTest");
+        return "+ FingerprintTest"
+    }).then(testPassed,testFailed)
+
+    .then(function(){
+        return browser.findElement(webdriver.By.css('tbody:nth-child(6) > tr:nth-child(2) > td:last-child > span')).getText();  
+    })
+    .then(function(length){
+        assert.equal(length, "138.3 мм","! LengthTest");
+        return "+ LengthTest"
+    }).then(testPassed,testFailed)
+
+    .then(function(){
+        return browser.findElement(webdriver.By.css('tbody:nth-last-child(6) > tr:nth-child(3) > td:last-child > span')).getAttribute('class'); 
+    })
+    .then(function(fmTuner){
+        assert.equal(fmTuner, "i-x","! FMtunerTest");
+        return "+ FMtunerTest"
+    }).then(testPassed,testFailed)
+
+    .then(function() {
+        browser.quit();
     });
-}
- function findPhone() {
-    return browser.findElements(webdriver.By.linkText("Apple iPhone 6s 16GB Silver")).then(function(result) {
-        return result[0];
-    });
-}
-function closeBrowser() {
-    browser.quit();
-}
- 
-browser.get('https://www.onliner.by');
-browser.findElement(webdriver.By.linkText("Мобильные телефоны")).click();
-browser.wait(findAppleCheckbox, 5000).then(clickLink);
-browser.wait(findPhone, 5000).then(clickLink).then(handleFailure);
